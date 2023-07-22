ed5d729c0668c536dc33b18a050a66ab06f2bfed
= <=>
2023-07-22 20:23:43 +0700

    add mod role

---
+++ src/Controller/Mod/OrderCrudController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller\Mod;
+
+use App\Entity\Order;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+
+class OrderCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Order::class;
+    }
+
+    public function configureActions(Actions $actions): Actions 
+    {
+        return $actions
+            ->add('index', 'detail')
+            ->remove(Crud::PAGE_INDEX, Action::NEW)
+            ;
+    }
+
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Order')
+            ->setEntityLabelInPlural('Orders')
+            ->setDefaultSort(['id' => 'DESC']);
+    }
+ 
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            DateTimeField::new('createdAt', 'Date'),
+            TextField::new('user.fullname', 'Name'),
+            TextField::new('user.email', 'Email'),
+            TextEditorField::new('address', 'Address')->onlyOnDetail(),
+            MoneyField::new('total')->setCurrency('USD')->hideOnForm(),
+            MoneyField::new('carrierPrice', 'Shipping fee')->setCurrency('USD'),
+            ChoiceField::new('state', 'Status')->setChoices([
+                'Unpaid' => 0,
+                'Paid' => 1,
+                'Preparation in progress' => 2,
+                'Shipped' => 3,
+            ]
+            ),
+            ArrayField::new('orderDetails', 'Purchased')->hideOnIndex()->hideOnForm()
+        ];
+    }
+
+}
